42635fc58023a956f64e4c4e3bb5db69db6d0352
joe vasquez <joe.vasquez@redwatchai.com>
2020-07-09 18:03:14 -0500

    working version

--- analyzers/HeadlessChromium/HeadlessChromium.py
+++ analyzers/HeadlessChromium/HeadlessChromium.py
@@ -4,6 +4,8 @@ import os
 import random
 import string
 import subprocess
+import tempfile
+from shutil import copyfileobj
 
 from cortexutils.analyzer import Analyzer
 from thehive4py.api import TheHiveApi
@@ -43,40 +45,66 @@ class HeadlessChromium(Analyzer):
         if self.service not in SERVICES:
             self.error("bad service")
 
+        self.filename = None
 
-def run(self):
-    if self.service == "screenshot":
-        letters = string.ascii_letters
-        tmp_profile_path = "/tmp/" + "".join(random.choice(letters) for i in range(13))
-        command_parts = [
-            self.binary_path,
-            "--headless",
-            "--user-data-dir=" + tmp_profile_path,
-            "--window-size=" + self.window_size,
-            "--user-agent=" + self.user_agent,
-            "--screenshot",
-            self.data,
-        ]
-        completed_process = subprocess.run(command_parts, capture_output=True)
-        if b"Written to file screenshot.png" not in completed_process.stdout:
-            self.error("Could not create screenshot.")
+    def summary(self, raw):
+        return {}
 
-        filename = os.path.join(self.cwd, "screenshot.png")
-        if not os.path.exists(filename):
-            self.error("Missing screenshot.")
+    def artifacts(self, raw):
+        if self.filename:
+            return [
+                self.build_artifact("file", self.filename),
+            ]
+        return []
 
-        api = TheHiveApi(self.thehive_url, self.thehive_apikey)
-        file_observable = CaseObservable(
-            dataType="file",
-            data=[filename,],
-            tlp=self.get_param("data.tlp"),
-            tags=["screenshot", "png", "parent:" + self.get_param("data.data"),],
-            message="retrieved through headless Chromium",
-        )
-        res = api.create_case_observable(
-            self.get_param("data._parent"), file_observable
-        )
-        self.report({"message": f"{res.status_code} {res.text}"})
+    def build_artifact(self, data_type, data, **kwargs):
+        if data_type == "file":
+            (dst, filename) = tempfile.mkstemp(
+                dir=os.path.join(self.job_directory, "output")
+            )
+            with open(data, "rb") as src:
+                copyfileobj(src, open(dst, "wb"))
+                kwargs.update(
+                    {
+                        "dataType": data_type,
+                        "file": os.path.basename(filename),
+                        "filename": os.path.basename(data),
+                    }
+                )
+                return kwargs
+
+        else:
+            kwargs.update({"dataType": data_type, "data": data})
+            return kwargs
+
+    def run(self):
+        if self.service == "screenshot":
+            filename = os.path.join(self.cwd, "screenshot.png")
+            if os.path.exists(filename):
+                os.remove(filename)
+
+            letters = string.ascii_letters
+            tmp_profile_path = "/tmp/" + "".join(
+                random.choice(letters) for i in range(13)
+            )
+            command_parts = [
+                self.binary_path,
+                "--headless",
+                "--user-data-dir=" + tmp_profile_path,
+                "--window-size=" + self.window_size,
+                f'--user-agent="{self.user_agent}"',
+                "--screenshot",
+                self.data,
+            ]
+            completed_process = subprocess.run(
+                command_parts, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+            )
+
+            if not os.path.exists(filename):
+                self.error("Missing screenshot.")
+            else:
+                self.filename = filename
+                self.report({"results": "created screenshot"})
 
 
 if __name__ == "__main__":
