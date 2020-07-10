e26df42f788332718bf9a0f3fb708a9869517a3c
joe vasquez <joe.vasquez@redwatchai.com>
2020-07-10 15:42:59 -0500

    added missing import

--- analyzers/HeadlessChromium/HeadlessChromium.py
+++ analyzers/HeadlessChromium/HeadlessChromium.py
@@ -7,6 +7,7 @@ import subprocess
 import tempfile
 from pathlib import Path
 from shutil import copyfileobj
+from urllib.parse import urlsplit
 
 import iocextract
 from cortexutils.analyzer import Analyzer
